4353ba2e685e90c4dcf0ed8998657259eb8f43d4
yasuomaidana <yasuomaidana@gmail.com>
2023-06-07 07:04:07 +0900

    COUR-1117: Profile component

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,25 +1,39 @@
-import React from 'react'
-import { Typography } from '@mui/material';
-import TimeLine from '../TimeLine/TimeLine';
-import './profile.scss'
+import React from "react";
+import { Typography } from "@mui/material";
+import TimeLine from "../TimeLine/TimeLine";
+import "./profile.scss";
+import WorkIcon from '@mui/icons-material/Work';
+import ProfileInfo from "./ProfileInfo";
 
 const Profile = () => {
+  const profile_info = [
+    <ProfileInfo title="Name" content="Yasuo Ignacio Maidana Perez" />,
+    <ProfileInfo title="Birthday" content="Yasuo Ignacio Maidana Perez" />,
+    <ProfileInfo title="Occupation" content="Masters degree student" />,
+    <ProfileInfo title="Email" content="Yasuo Ignacio Maidana Perez" />,
+  ];
   return (
-    <div className='profile container_shadow'>
-        <div className='profile_name'>
-          <Typography className='name'>Maidana Perez Yasuo Ignacio</Typography>
-          <Typography className='title'>Master of Engineering (M1)</Typography>
-        </div>
-        <figure className='profile_image'>
-          <img src="./assets/images/profile picture.jpg" alt="Profile"/>
-        </figure>
+    <div className="profile container_shadow">
+      <div className="profile_name">
+        <Typography className="name">Yasuo Maidana</Typography>
+        <Typography className="title">Master of Engineering (M1)</Typography>
+      </div>
+      <figure className="profile_image">
+        <img
+          src={require("../../assets/images/profile picture.jpg")}
+          alt="Profile"
+        />
+      </figure>
 
-        <div className='profile_information'>
-          Insert <TimeLine></TimeLine>
-
-        </div>
+      <div className="profile_information">
+        <TimeLine
+          title="Yasuo Ignacio Maidana Pérez"
+          children={profile_info}
+          icon={<WorkIcon />}
+        ></TimeLine>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
