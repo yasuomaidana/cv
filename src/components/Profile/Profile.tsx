8233ffc65c1609a2cdd7a8e71f05aadd35b031f0
yasuomaidana <yasuomaidana@gmail.com>
2023-06-07 02:35:10 +0900

    COUR-1117: Completing profile

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Typography } from '@mui/material';
+import TimeLine from '../TimeLine/TimeLine';
+import './profile.scss'
 
 const Profile = () => {
   return (
     <div className='profile container_shadow'>
-        <div>Hi</div>
+        <div className='profile_name'>
+          <Typography className='name'>Maidana Perez Yasuo Ignacio</Typography>
+          <Typography className='title'>Master of Engineering (M1)</Typography>
+        </div>
+        <figure className='profile_image'>
+          <img src="./assets/images/profile picture.jpg" alt="Profile"/>
+        </figure>
+
+        <div className='profile_information'>
+          Insert <TimeLine></TimeLine>
+
+        </div>
     </div>
   )
 }
