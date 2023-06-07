8e46650df3dabafa2e0b84b65c9f23f01e147f9a
yasuomaidana <yasuomaidana@gmail.com>
2023-06-08 00:04:17 +0900

    COUR-1117: Creating header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { HomeRounded } from "@mui/icons-material";
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import { NavLink } from 'react-router-dom';
+import socials from "../../utils/socials";
+import { SocialComponent } from "./SocialComponent/SocialComponent";
 
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
+interface HeaderProp {
+  pathname: string;
 }
-
-export default Header
+const Header: React.FC<HeaderProp> = (headerProp) => {
+  const pathName = headerProp.pathname;
+  
+  return (
+    <Navbar sticky="top" expand="lg" className="header">
+      <Nav.Link as={NavLink} to="/">
+        <Navbar.Brand className="header_home">
+          <HomeRounded />
+        </Navbar.Brand>
+      </Nav.Link>
+      <Navbar.Toggle />
+      <Navbar.Collapse>
+        <Nav>
+          <Nav.Link
+            as={NavLink}
+            to="/"
+            className={pathName === "/" ? "active" : ""}
+          >
+            Home
+          </Nav.Link>
+          <Nav.Link
+            as={NavLink}
+            to="/portfolio"
+            className={pathName === "/portfolio" ? "active" : ""}
+          >
+            Portfolio
+          </Nav.Link>
+        </Nav>
+        <div className="header_right">
+        {Object.entries(socials).map(([k,v],i) => <SocialComponent key={i.toString()} social={v}/>)}
+        </div>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+export default Header;
