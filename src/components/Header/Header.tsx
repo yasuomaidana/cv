d0ba6200bdd3fc732cf9886fcaa6443a6370f32f
yasuomaidana <yasuomaidana@gmail.com>
2023-06-08 02:32:07 +0900

    COUR-1117: Polishing header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,8 +5,6 @@ import { NavLink } from "react-router-dom";
 import socials from "../../utils/socials";
 import { SocialComponent } from "./SocialComponent/SocialComponent";
 import "./Header.scss";
-import { useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
 
 const Header = ( ) => {
 
