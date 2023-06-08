f35cd1bb149e9bda1b9a00607d8e3f4063331f0e
yasuomaidana <yasuomaidana@gmail.com>
2023-06-09 01:46:58 +0900

    COUR-1117: Refactoring Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,12 @@
-import { Grid, Typography } from "@mui/material";
 import React from "react";
 import "./Home.scss";
+import BoxWithTitle from "../../components/BoxWithTitle/BoxWithTitle";
+import { about_me } from "../../utils/about_me_info";
 const Home = () => {
   return (
     <>
-      <Grid container className="section">
+      <BoxWithTitle title="About me" content={about_me}/>
+      {/* <Grid container className="section">
         <div className=" mb_30">
           <Grid item className="section_title">
             <span></span>
@@ -15,40 +17,15 @@ const Home = () => {
         </div>
         <Grid item xs={12}>
           <Typography variant="body2" component={"div"} className={"about_me_text"}>
-            <p >
-              I'm Yasuo an engineer that loves programming, robotics, research,
-              3D printing ... and breaking things.
-              <br />
-              In few words, I enjoy learning new stuffs. I would like to think
-              about myself as a scientist, at least I pursuing that spirit
-              <br />
-              If I should define me in few words, I would say that I'm a person
-              who believe that through studying and compassion we can enhance
-              our world.
-              <br />
-            </p>
-            <ul>
-              <li>
-                Studying to understand how our world works, the reason why the
-                things are the way they are, and how to enhance it.
-              </li>
-              <li>
-                And compassion to integrate ourselfs and others to society.
-              </li>
-            </ul>
+            
           </Typography>
         </Grid>
-      </Grid>
-
-      <Grid container className="section">
-        Education
-      </Grid>
-      <Grid container className="section">
-        Skills
-      </Grid>
-      <Grid container className="section">
-        Contact
-      </Grid>
+      </Grid> */}
+      <hr/>
+      <BoxWithTitle title="Education" content={<>Random jijitl help me!!!</>}/>
+      <hr/>
+      <BoxWithTitle title="Skills" content={<>Random jijitl help me!!!</>}/>
+      <hr/>
     </>
   );
 };
---
+++ src/utils/about_me_info.tsx
@@ -0,0 +1,22 @@
+export const about_me = (
+  <>
+    <p>
+      I'm Yasuo an engineer that loves programming, robotics, research, 3D
+      printing ... and breaking things.
+      <br />
+      In few words, I enjoy learning new stuffs. I would like to think about
+      myself as a scientist, at least I pursuing that spirit
+      <br />
+      If I should define me in few words, I would say that I'm a person who
+      believe that through studying and compassion we can enhance our world.
+      <br />
+    </p>
+    <ul>
+      <li>
+        Studying to understand how our world works, the reason why the things
+        are the way they are, and how to enhance it.
+      </li>
+      <li>And compassion to integrate ourselfs and others to society.</li>
+    </ul>
+  </>
+);
