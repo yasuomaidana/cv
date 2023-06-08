4d79f9715ae794486e46aca579b2b4ced34f2d31
yasuomaidana <yasuomaidana@gmail.com>
2023-06-08 20:44:20 +0900

    COUR-1117: Adding resume and home

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import { Grid } from '@mui/material'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+        <Grid container className="">aa</Grid>
+    </>
+  )
+}
+
+export default Home
