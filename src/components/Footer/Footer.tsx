1be0b9ffd1f5f9848f5bed4b3ceb5d2f28f90b11
yasuomaidana <yasuomaidana@gmail.com>
2023-06-08 20:30:19 +0900

    COUR-1117: Finishing footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { Typography } from '@mui/material'
+import CodeIcon from '@mui/icons-material/Code';
+import "./Footer.scss"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className='left'>
+        <Typography className='footer_name'>Yasuo Maidana</Typography>
+      </div>
+      <div className='right'>
+        <Typography className='footer_copyright'>
+          Designed and Developed by <a href='/'>Y.M.</a>
+          <br/>
+          Source code<a href='https://github.com/yasuomaidana/cv' target="_blank" rel="noopener noreferrer"><CodeIcon/>.</a>
+        </Typography>
+      </div>
+    </div>
   )
 }
 
