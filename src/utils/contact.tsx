d2b21bba050d00caa4769cd394853a6d1c46294c
yasuomaidana <yasuomaidana@gmail.com>
2023-06-09 07:47:15 +0900

    COUR-1117: Fixing birthday

--- src/utils/contact.tsx
+++ src/utils/contact.tsx
@@ -3,7 +3,7 @@ export const contact = {
     short_name: "Yasuo I. Maidana P.",
     shortest_name: "Yasuo Maidana",
     email: "yasuo.maidana@vulpuxmecatroncus.com",
-    birthday: "06 June 1996",
+    birthday: "22 June 1996",
     major: "Mechatronics Engineering",
     job: "Master's of Engineering (M1) \nShizuoka University, Japan"
 }
