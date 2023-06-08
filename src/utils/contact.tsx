3e88e18ab243db53aafa100679cced889f476762
yasuomaidana <yasuomaidana@gmail.com>
2023-06-09 03:03:23 +0900

    COUR-1117: Fixing typing mistake

--- src/utils/contact.tsx
+++ src/utils/contact.tsx
@@ -4,7 +4,7 @@ export const contact = {
     shortest_name: "Yasuo Maidana",
     email: "yasuo.maidana@vulpuxmecatroncus.com",
     birthday: "06 June 1996",
-    major: "Mechatronics Engineer",
+    major: "Mechatronics Engineering",
     job: "Master's of Engineering (M1) \nShizuoka University, Japan"
 }
 
