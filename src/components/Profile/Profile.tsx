bf5dc378356e3a49bf47cf754e13e6f3bfa00a03
yasuomaidana <yasuomaidana@gmail.com>
2023-06-09 05:08:14 +0900

    COUR-1117: Adding frame to picture

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -3,28 +3,37 @@ import { Link, Typography } from "@mui/material";
 import TimeLine from "../TimeLine/TimeLine";
 import "./profile.scss";
 import ProfileInfo from "./ProfileInfo";
-import ContactsIcon from '@mui/icons-material/Contacts';
-import {contact} from "../../utils/contact";
+import ContactsIcon from "@mui/icons-material/Contacts";
+import { contact } from "../../utils/contact";
 const Profile = () => {
-  
   const profile_info = [
     <ProfileInfo key={1} title="Name" content={<>{contact.full_name}</>} />,
     <ProfileInfo key={2} title="Birthday" content={<>{contact.birthday}</>} />,
     <ProfileInfo key={3} title="Major" content={<>{contact.major}</>} />,
-    <ProfileInfo  key={4} title="Email" content={<Link href={"mailto:"+contact.email}>{contact.email.replace(/@[^@]+$/, '')}</Link>}/> ];
+    <ProfileInfo
+      key={4}
+      title="Email"
+      content={
+        <Link href={"mailto:" + contact.email}>
+          {contact.email.replace(/@[^@]+$/, "")}
+        </Link>
+      }
+    />,
+  ];
   return (
     <div className="profile container_shadow">
       <div className="profile_name">
         <Typography className="name">{contact.shortest_name}</Typography>
         <Typography className="title">{contact.job}</Typography>
       </div>
-      <figure className="profile_image">
-        <img
-          src={require("../../assets/images/profile picture.jpg")}
-          alt="Profile"
-        />
-      </figure>
-
+      <div className="profile_image_frame">
+        <figure className="profile_image">
+          <img
+            src={require("../../assets/images/profile picture.jpg")}
+            alt="Profile"
+          />
+        </figure>
+      </div>
       <div className="profile_information">
         <TimeLine
           title={contact.full_name}
