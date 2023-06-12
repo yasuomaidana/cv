f58cf75b1ee6dc4a4dabc30618152ef373b97c96
yasuomaidana <yasuomaidana@gmail.com>
2023-06-12 15:35:22 +0900

    Starting portfolio page

--- src/pages/Portfolio/Portfolio.tsx
+++ src/pages/Portfolio/Portfolio.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CV from '../../CV'
+import BoxWithTitle from '../../components/BoxWithTitle/BoxWithTitle'
+import { Grid, Tab, Tabs } from '@mui/material';
+import { PortFolioItem, PortfolioTag, portfolioItems } from '../../utils/portfolio';
+
+
 
 const Portfolio = () => {
+  const [tabValue, setTabValue] = useState("All");
+
+  const renderTagFromTab = (tab:string, key:number) => <Tab key={key} label = {tab} value  = {tab} className={tabValue === tab ? 'custom_item active': "custom_item"}/>
+  
+  const renderAll = (items:PortFolioItem[]) =>{
+    
+  }
+  const portfolioContainer = (selectedTab:string = "All") => 
+  <Grid item>
+    <Tabs value={selectedTab} indicatorColor='secondary' 
+    className='custom_tabs' onChange={(event,newValue)=> setTabValue(newValue)}>
+      <Tab label = 'All' value  = "All" className={tabValue === 'All' ? 'custom_item active': "custom_item"}/>
+      { Object.values(PortfolioTag).map((tag, index) => renderTagFromTab(tag, index))}
+    </Tabs>
+  </Grid>;   
+  
   return (
-    <CV/>
+    <BoxWithTitle title='Portfolio' content={portfolioContainer(tabValue)}/>
   )
 }
 
