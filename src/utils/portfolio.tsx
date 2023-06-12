6abb790bd11b7110a523d68dc2d61dca8f2e9b68
yasuomaidana <yasuomaidana@gmail.com>
2023-06-12 15:35:09 +0900

    COUR-1117: Defining portfolio info

---
+++ src/utils/portfolio.tsx
@@ -0,0 +1,30 @@
+import { SvgIconComponent, YouTube } from "@mui/icons-material";
+
+export enum PortfolioTag{
+    Coding = "Coding",
+    Robotics = "Robotics",
+    Electronics = "Electronics"
+}
+
+export interface PortFolioItem {
+    tags : PortfolioTag[];
+    portfolio_image: string;
+    video?: string;
+    title: string;
+    description: string;
+    links? : {link:string; icon:React.ReactElement<SvgIconComponent>}[]
+}
+export const portfolioItems:PortFolioItem[] = [
+    {
+        tags: [PortfolioTag.Robotics, PortfolioTag.Coding, PortfolioTag.Electronics],
+        portfolio_image: "",
+        title: "Pick and Place Machine",
+        description: "This prototype is capable of holding electronic components and placing them into the working area. We incorporated a laser led to simulate micro soldering using laser technique",
+        links: [
+            {
+                link:"https://www.youtube.com/watch?v=HjTVGrmYySE&ab_channel=YasuoIgnacioMaidanaP%C3%A9rez",
+                icon: <YouTube/>
+            }
+        ]
+    }
+];
