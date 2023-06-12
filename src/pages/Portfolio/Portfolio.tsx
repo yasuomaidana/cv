9c2381053c28fb4c11e6081a60fe50e6b1db8e86
yasuomaidana <yasuomaidana@gmail.com>
2023-06-12 16:05:13 +0900

    COUR-1117: Adding custom tag and enhancing presentation

--- src/pages/Portfolio/Portfolio.tsx
+++ src/pages/Portfolio/Portfolio.tsx
@@ -3,6 +3,8 @@ import CV from '../../CV'
 import BoxWithTitle from '../../components/BoxWithTitle/BoxWithTitle'
 import { Grid, Tab, Tabs } from '@mui/material';
 import { PortFolioItem, PortfolioTag, portfolioItems } from '../../utils/portfolio';
+import PortfolioItemComponent from './PortfolioItemComponent';
+import "./Portfolio.scss"
 
 
 
@@ -17,10 +19,11 @@ const Portfolio = () => {
   const portfolioContainer = (selectedTab:string = "All") => 
   <Grid item>
     <Tabs value={selectedTab} indicatorColor='secondary' 
-    className='custom_tabs' onChange={(event,newValue)=> setTabValue(newValue)}>
+    className='custom_tabs' onChange={(event,newValue)=> setTabValue(newValue)} centered>
       <Tab label = 'All' value  = "All" className={tabValue === 'All' ? 'custom_item active': "custom_item"}/>
       { Object.values(PortfolioTag).map((tag, index) => renderTagFromTab(tag, index))}
     </Tabs>
+    <PortfolioItemComponent/>
   </Grid>;   
   
   return (
---
+++ src/pages/Portfolio/PortfolioItemComponent.tsx
@@ -0,0 +1,12 @@
+import { Grid } from '@mui/material'
+import React from 'react'
+
+const PortfolioItemComponent = () => {
+  return (
+    <Grid container className='section pb_45 pt_45'>
+        Hi
+    </Grid>
+  )
+}
+
+export default PortfolioItemComponent
