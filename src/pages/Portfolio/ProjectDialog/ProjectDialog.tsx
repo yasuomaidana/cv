ddd7ff8c4475e82d3ac938711cfe9323f07cc060
yasuomaidana <yasuomaidana@gmail.com>
2023-06-13 18:20:35 +0900

    COUR-1117: Enhancing presentation

--- src/pages/Portfolio/ProjectDialog/ProjectDialog.tsx
+++ src/pages/Portfolio/ProjectDialog/ProjectDialog.tsx
@@ -1,4 +1,4 @@
-import { Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material'
+import { Dialog, DialogActions, DialogContent, DialogTitle, Typography } from '@mui/material'
 import React from 'react'
 import { PortFolioItem } from '../../../utils/portfolio';
 import PortfolioMediaGallery from './MediaGallery/PortfolioMediaGallery';
@@ -14,10 +14,10 @@ const ProjectDialog:React.FC<ProjectDialogProps> = (props) => {
     const item = props.item
   return (
     <Dialog open={props.open} onClose={props.handle_toggle} fullWidth maxWidth="lg">
-        <DialogTitle>{item.title}</DialogTitle>
+        <DialogTitle component={"span"} className='project_dialog_title'><Typography variant='h3' textAlign={"center"}>{item.title}</Typography></DialogTitle>
         {props.item.portFolioMedia && <PortfolioMediaGallery props={item.portFolioMedia}/>}
         <DialogContent>
-            {item.description}
+            <Typography className='project_dialog_content' variant='body1'>{item.description}</Typography>
         </DialogContent>
         {<DialogActions className='project_dialog_actions'>
             {item.links?.map((link,key)=> 
