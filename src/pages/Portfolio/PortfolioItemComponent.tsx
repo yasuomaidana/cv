73b181a9a57050327a4e6e329d3573612c412485
yasuomaidana <yasuomaidana@gmail.com>
2023-06-13 13:46:59 +0900

    COUR-1117: Enhancing presentation

--- src/pages/Portfolio/PortfolioItemComponent.tsx
+++ src/pages/Portfolio/PortfolioItemComponent.tsx
@@ -1,42 +1,70 @@
-import { Card, CardActionArea, CardContent, CardMedia, Grid, Grow, Typography } from '@mui/material'
-import React from 'react'
-import { PortFolioItem } from '../../utils/portfolio'
-import { Code, SvgIconComponent } from '@mui/icons-material'
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Grid,
+  Grow,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import { PortFolioItem } from "../../utils/portfolio";
+import { Code, SvgIconComponent } from "@mui/icons-material";
+import "./PortfolioItemComponent.scss";
 
-interface PortfolioItemComponentProps{
-  item:PortFolioItem;
+interface PortfolioItemComponentProps {
+  item: PortFolioItem;
   handleToggle: () => void;
-  handlePortfolioSelection: (selectedPortfolio:PortFolioItem) => void;
-
+  handlePortfolioSelection: (selectedPortfolio: PortFolioItem) => void;
 }
 
-
-
-const renderMedia = (media_item: string | React.ReactElement<SvgIconComponent>, title:string = "") => 
-typeof(media_item) === "string"? 
-  <CardMedia className='portfolio_card_image' component={"img"} image={media_item} sx={{ height: 140}}/>:
-  <CardMedia component={Code} sx={{ height: 140 , margin: 'auto', fontSize: '5rem'}} />
-const PortfolioItemComponent:React.FC<PortfolioItemComponentProps> = (props,  key) => {
+const renderMedia = (
+  media_item: string | React.ReactElement<SvgIconComponent>,
+  title: string = ""
+) =>
+  typeof media_item === "string" ? (
+    <CardMedia
+      className="portfolio_card_image"
+      component={"img"}
+      image={media_item}
+      sx={{ height: 140 }}
+      alt={title}
+    />
+  ) : (
+    <CardMedia
+      component={Code}
+      sx={{ height: 140, margin: "auto", fontSize: "5rem" }}
+    />
+  );
+const PortfolioItemComponent: React.FC<PortfolioItemComponentProps> = (
+  props,
+  key
+) => {
   const item = props.item;
-  const handleClick = (props:PortfolioItemComponentProps) => {
+  const handleClick = (props: PortfolioItemComponentProps) => {
     props.handleToggle();
     props.handlePortfolioSelection(item);
-  }
+  };
+  const max_length = 180;
   return (
-      <Grid item key={key} md={4}>
-        <Grow in timeout={1000}>
-        <Card onClick={()=> handleClick(props)}>
+    <Grid item key={key} md={4}>
+      <Grow in timeout={1000}>
+        <Card className="portfolio_card" onClick={() => handleClick(props)}>
           <CardActionArea>
             {renderMedia(item.thumbnail)}
             <CardContent>
-              <Typography className='portfolio_title'>{item.title}</Typography>
-              <Typography variant="body2" className='portfolio_description'>{item.description}</Typography>
+              <Typography className="portfolio_title">{item.title}</Typography>
+              <Typography variant="body2" className="portfolio_description">
+                {item.description.length <= max_length
+                  ? item.description
+                  : item.description.substring(0, max_length) + " ..."}
+              </Typography>
             </CardContent>
           </CardActionArea>
         </Card>
-        </Grow>
+      </Grow>
     </Grid>
-  )
-}
+  );
+};
 
-export default PortfolioItemComponent
+export default PortfolioItemComponent;
