0441e189adc52e67e781afcb76bd2ce73fa6bb37
yasuomaidana <yasuomaidana@gmail.com>
2023-06-18 19:13:26 +0900

    COUR-1117: Updating about me

--- src/utils/about_me_info.tsx
+++ src/utils/about_me_info.tsx
@@ -3,22 +3,33 @@ import Typography from "@mui/material/Typography";
 export const about_me = (
   <Typography variant="body2" component={"div"} className={"about_me_text"}>
     <p>
-      I'm Yasuo an engineer that loves programming, robotics, research, 3D
-      printing ... and breaking things.
+      I'm an engineer that loves programming, electronics, robotics, research,
+      3D printing ... and breaking things.
       <br />
-      In few words, I enjoy learning new stuffs. I would like to think about
-      myself as a scientist, at least I pursuing that spirit
+      In a few words, I enjoy learning new stuff. I would like to think about
+      myself as a scientist, or at least I'm pursuing the spirit.
       <br />
-      If I should define me in few words, I would say that I'm a person who
-      believe that through studying and compassion we can enhance our world.
+      I believe that through science and compassion, we can enhance our world.
       <br />
     </p>
     <ul>
       <li>
-        Studying to understand how our world works, the reason why the things
-        are the way they are, and how to enhance it.
+        Science to understand how our world works, the reason why things are the
+        way they are, and how to control/enhance them.
       </li>
-      <li>And compassion to integrate ourselfs and others to society.</li>
+      <li> And compassion to integrate ourselves and others into society.</li>
     </ul>
+    <p>
+      I enjoy programming too much that I worked professionally in that industry
+      for a while. My first contact with programming was through simulations,
+      and since then it has become one of my favorite hobbies.
+      <br />I also love machine learning-related topics, I found very
+      interesting the analysis of our way of thinking and how to replicate it.
+    </p>
+
+    <p>
+      I'm always looking for opportunities/exciting projects where I can learn
+      new things and mix my current knowledge.
+    </p>
   </Typography>
 );
